10550f645214c652bc37ddbc1ed3696d5d603084
Volodymyr Molchanov <volmolchanov1998@gmail.com>
2025-07-13 20:26:33 +0300

    feature: setup shadcn and vite - implement login form design - implement theming and its toggling

---
+++ client/src/components/theme/theme-provider.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import type { Theme, ThemeProviderProps } from "./theme-types";
+import { ThemeProviderContext } from "./theme-context";
+import { THEME_STORAGE_KEY } from "@/constants/theme-constants";
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "system",
+  storageKey = THEME_STORAGE_KEY,
+  ...props
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
+
+  return (
+    <ThemeProviderContext.Provider {...props} value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
+}
